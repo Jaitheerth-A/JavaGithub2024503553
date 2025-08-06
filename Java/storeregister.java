86e3e644c7d28dc549ee02e3cf037772e09c88d8
Jaitheerth-A <jaitheerth21@gmail.com>
2025-08-06 11:53:20 +0530

    added a store class

---
+++ Java/storeregister.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class register
+{
+    public
+    int custid[];String product[][];float cost[][];
+    register(int n){custid=new int[n];product=new String[n][];cost=new float[n][];}
+}
+class customer
+{
+    customer(){}
+    customer(int n){
+    register r=new register(n);
+    }
+    void getinfo()
+    {
+                
+    }
+} 
+public class storeregister
+{
+    public static void main()
+}
