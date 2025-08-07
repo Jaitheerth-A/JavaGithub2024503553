fb1c476e6be8fb1019ab86b6df1463400cc8747d
Jaitheerth-A <jaitheerth21@gmail.com>
2025-08-07 10:33:33 +0530

    Update storeregister.java

--- Java/storeregister.java
+++ Java/storeregister.java
@@ -7,16 +7,30 @@ class register
 }
 class customer
 {
+    register r;
+    static int count;
+    //constructors
     customer(){}
-    customer(int n){
-    register r=new register(n);
-    }
-    void getinfo()
+    customer(int n)
     {
-                
+
+    r=new register(n);
+    int id;
+    System.out.print("Enter the id number of "+ suffix(i+1));
+    for(int i=0;i<n;i++)
+    {getinfo(id);}
+
+    }
+    //methods
+
+    String suffix(int i){}
+    void getinfo(int id)
+    {
+                r.custid[count++]=id;
     }
 } 
 public class storeregister
 {
     public static void main()
+    {}
 }
